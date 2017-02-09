fe2a5fc546d0362349cf1dad8047e149ac2ff271
Julija Lee <lee.julija@gmail.com>
2017-02-09 21:17:36 +0000

    changed number of strings

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	ld = n % 10;
+	printf("Last digit of %d ", n);
+	printf("is ");
 	if (ld == 0)
-		printf("Last digit of %d is %d and is zero\n", n, ld);
+		printf("%d and is zero\n", ld);
 	else
 	if (ld > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+		printf("%d and is greater than 5\n", ld);
 	else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("%d and is less than 6 and not 0\n", ld);
 	return (0);
 }
