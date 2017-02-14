e8f2429c38dd6defdb89c5d95fd233526483354b
Julija Lee <lee.julija@gmail.com>
2017-02-14 22:09:45 +0000

    adding 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: secon number
+ *
+ * holberton.h: created header
+ *
+ * Return: result
+**/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
