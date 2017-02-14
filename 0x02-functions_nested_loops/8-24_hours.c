0b784fccf44e8e026415141f8b6bed8a26b224d8
Julija Lee <lee.julija@gmail.com>
2017-02-14 21:11:40 +0000

    adding 7 and 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints every minute of 24 hrs
+ * @void: no parameters
+ *
+ * holberton.h: created header
+ *
+ * Return: none
+**/
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
