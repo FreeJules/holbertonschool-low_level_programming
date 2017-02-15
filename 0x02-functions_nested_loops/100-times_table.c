d10d33445dc1a6e5c462341fe3deaff6cf8dc0b2
Julija Lee <lee.julija@gmail.com>
2017-02-15 04:42:35 +0000

    trying it again - 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,17 +9,17 @@
 **/
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j, k, d, m;
 
 	if (n < 0 || n > 15)
 		return;
-	i = 0;
-	while (i <= n)
+	for (i = 0; i <= n; i++)
 	{
-		j = 0;
-		k = 0;
-		while (j <= n)
+		for (j = 0; j <= n; j++)
 		{
+			k = i * j;
+			d = k / 10;
+			m = k % 10;
 			if (k < 1000 && j != 0)
 				_putchar(' ');
 			if (k < 100 && j != 0)
@@ -28,23 +28,20 @@ void print_times_table(int n)
 				_putchar(' ');
 			if (k >= 100)
 			{
-				_putchar(k / 100 + '0');
-				_putchar((k / 10) % 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar(d / 10 + '0');
+				_putchar(d % 10 + '0');
+				_putchar(m + '0');
 			}
 			if (k >= 10 && k < 100)
 			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
+				_putchar(d + '0');
+				_putchar(m + '0');
 			}
 			if (k < 10)
 				_putchar(k + '0');
 			if (j != n)
 				_putchar(',');
-			k = k + i;
-			j++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
