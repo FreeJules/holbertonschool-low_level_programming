4e1d5c4a3dec647d910ca5aed8a3768e50b6710b
Julija Lee <lee.julija@gmail.com>
2017-02-17 08:18:53 +0000

    last one

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,14 +8,14 @@
  */
 void print_number(int n)
 {
-	int div, i, first_non_zero, last_digit;
+	int div, first_non_zero, last_digit;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	div = 100000000;
+	div = 1000000000;
 	last_digit = n % 10;
 	n = (n / 10);
 	if (n < 0)
@@ -24,9 +24,8 @@ void print_number(int n)
 		n = n * (-1);
 		last_digit = last_digit * (-1);
 	}
-	i = 0;
 	first_non_zero = 0;
-	while (i < 9)
+	while (div >= 1)
 	{
 		if (n / div != 0)
 			first_non_zero = n / div;
@@ -34,7 +33,6 @@ void print_number(int n)
 			_putchar(n / div + '0');
 		n = n % div;
 		div /= 10;
-		i++;
 	}
 	_putchar(last_digit + '0');
 }
