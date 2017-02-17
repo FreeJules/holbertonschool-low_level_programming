bd136a7a1e9692ea01479b69af45464d8d23122e
Julija Lee <lee.julija@gmail.com>
2017-02-17 04:19:39 +0000

    calculated div instead of making it 1000000000

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,28 +8,43 @@
  */
 void print_number(int n)
 {
-	int div, i, first_digit, last_digit;
+	int nl, lenght, j, div;
+	int i, last_digit;
 
-	div = 100000000;
+/* calculating last digit and dividing n by 10 to be able to
+ * change negative to positive without getting out of range
+ */
 	last_digit = n % 10;
 	n = (n / 10);
-	first_digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * (-1);
 		last_digit = last_digit * (-1);
 	}
-	i = 0;
-	while (i < 9)
+/* calculating lenght of new n */
+	nl = n;
+	lenght = 0;
+	while (nl > 0)
 	{
-		if (n / div != 0)
-			first_digit = n / div;
-		if (first_digit != 0)
-			_putchar(n / div + '0');
+		lenght += 1;
+		nl = nl / 10;
+	}
+/* creating exponent for base 10 to divide with
+ * which will be one less than lenght so that we don't get 0 when we divide
+ */
+	div = 1;
+	for (j = 0; j < lenght - 1; j++)
+		div *= 10;
+/* printing number now that we have lenght and div */
+	i = 0;
+	while (i < lenght)
+	{
+		_putchar(n / div + '0');
 		n = n % div;
 		div = div / 10;
 		i++;
 	}
+/* print last digit (this covers 0 too)*/
 	_putchar(last_digit + '0');
 }
