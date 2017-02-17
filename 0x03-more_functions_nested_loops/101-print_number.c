efa3bca76bd2956be2794789b1cd2d18a7e1e52f
Julija Lee <lee.julija@gmail.com>
2017-02-17 08:07:49 +0000

    last try

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,8 +8,14 @@
  */
 void print_number(int n)
 {
-	int last_digit, tmp, lenght, div, i, j;
+	int div, i, first_non_zero, last_digit;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	div = 100000000;
 	last_digit = n % 10;
 	n = (n / 10);
 	if (n < 0)
@@ -18,29 +24,17 @@ void print_number(int n)
 		n = n * (-1);
 		last_digit = last_digit * (-1);
 	}
-/* calculating lenght of new n */
-	tmp = n;
-	lenght = 0;
-	while (tmp > 0)
+	i = 0;
+	first_non_zero = 0;
+	while (i < 9)
 	{
-		lenght += 1;
-		tmp = tmp / 10;
-	}
-/* creating exponent for base 10 to divide with
- * which will be one less than lenght so that we don't get 0 when we divide
- */
-	div = 1;
-	for (i = 0; i < lenght - 1; i++)
-		div *= 10;
-/* printing number now that we have lenght and div */
-	j = 0;
-	while (j < lenght)
-	{
-		_putchar(n / div + '0');
+		if (n / div != 0)
+			first_non_zero = n / div;
+		if (first_non_zero != 0)
+			_putchar(n / div + '0');
 		n = n % div;
-		div = div / 10;
-		j++;
+		div /= 10;
+		i++;
 	}
-/* print last digit */
 	_putchar(last_digit + '0');
 }
