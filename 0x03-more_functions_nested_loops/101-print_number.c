d15d1d47580cb9f6ab1b6347ac01b85676c250db
Julija Lee <lee.julija@gmail.com>
2017-02-17 02:52:23 +0000

    don't know why 101 is not working

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,27 +8,26 @@
  */
 void print_number(int n)
 {
-	int start, div, i, first_non_zero, last_digit;
+	int div, i, first_digit, last_digit;
 
 	div = 100000000;
 	last_digit = n % 10;
 	n = (n / 10);
+	first_digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * (-1);
 		last_digit = last_digit * (-1);
 	}
-	start = n;
 	i = 0;
-	first_non_zero = 0;
 	while (i < 9)
 	{
-		if (start / div != 0)
-			first_non_zero = start / div;
-		if (first_non_zero != 0)
-			_putchar(start / div + '0');
-		start = start % div;
+		if (n / div != 0)
+			first_digit = n / div;
+		if (first_digit != 0)
+			_putchar(n / div + '0');
+		n = n % div;
 		div = div / 10;
 		i++;
 	}
