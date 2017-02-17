0c47c4ba47a2faf5f7ff4dfd713737bcc2cad836
Julija Lee <lee.julija@gmail.com>
2017-02-17 07:44:36 +0000

    last try for 101, officially giving upgit status :(

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,12 +8,8 @@
  */
 void print_number(int n)
 {
-	int nl, lenght, j, div;
-	int i, last_digit;
+	int last_digit, tmp, lenght, div, i, j;
 
-/* calculating last digit and dividing n by 10 to be able to
- * change negative to positive without getting out of range
- */
 	last_digit = n % 10;
 	n = (n / 10);
 	if (n < 0)
@@ -23,28 +19,28 @@ void print_number(int n)
 		last_digit = last_digit * (-1);
 	}
 /* calculating lenght of new n */
-	nl = n;
+	tmp = n;
 	lenght = 0;
-	while (nl > 0)
+	while (tmp > 0)
 	{
 		lenght += 1;
-		nl = nl / 10;
+		tmp = tmp / 10;
 	}
 /* creating exponent for base 10 to divide with
  * which will be one less than lenght so that we don't get 0 when we divide
  */
 	div = 1;
-	for (j = 0; j < lenght - 1; j++)
+	for (i = 0; i < lenght - 1; i++)
 		div *= 10;
 /* printing number now that we have lenght and div */
-	i = 0;
-	while (i < lenght)
+	j = 0;
+	while (j < lenght)
 	{
 		_putchar(n / div + '0');
 		n = n % div;
 		div = div / 10;
-		i++;
+		j++;
 	}
-/* print last digit (this covers 0 too)*/
+/* print last digit */
 	_putchar(last_digit + '0');
 }
