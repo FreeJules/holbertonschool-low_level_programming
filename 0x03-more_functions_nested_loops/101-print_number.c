6550c2028fe011486d712372ed143e07e8bcd43c
Julija Lee <lee.julija@gmail.com>
2017-02-18 00:36:46 +0000

    improved 101 and added comments

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,23 @@
  */
 void print_number(int n)
 {
-	int div, first_non_zero, last_digit, tmp, lenght;
+	int div, last_digit, tmp, lenght;
 
+	/* calcualating last digit */
 	last_digit = n % 10;
+	/* if negative print '-' and make last digit positive */
 	if (last_digit < 0)
 	{
 		_putchar('-');
 		last_digit = last_digit * (-1);
 	}
+	/* calculate the number without the last digit and make positive */
 	n = (n / 10);
 	if (n < 0)
 	{
 		n = n * (-1);
 	}
+	/* calculate the length of a number and div to divide it with */
 	lenght  = 0;
 	div = 1;
 	tmp = n;
@@ -30,13 +34,11 @@ void print_number(int n)
 		div *= 10;
 		tmp = tmp / 10;
 	}
-	first_non_zero = 0;
+	/* make div one digit shorter than lenght */
+	div = div / 10;
 	while (div >= 1)
 	{
-		if (n / div != 0)
-			first_non_zero = n / div;
-		if (first_non_zero != 0)
-			_putchar(n / div + '0');
+		_putchar(n / div + '0');
 		n = n % div;
 		div /= 10;
 	}
