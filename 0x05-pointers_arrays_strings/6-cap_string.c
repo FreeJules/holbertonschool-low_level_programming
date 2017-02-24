87f53d9f2d14c59b20cda62cc9045de76564363e
Julija Lee <lee.julija@gmail.com>
2017-02-24 03:13:04 +0000

    fixed betty for 6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * cap-string - capitalizes all words of a string
+ * cap_string - capitalizes all words of a string
  * @s: string
  *
  * Return: new string
@@ -8,13 +8,13 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char c[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char c[] = " \t\n,;.!?\"(){}";
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
-		while (j < 13)
+		while (c[j] != '\0')
 		{
 			if (s[i] == c[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] = s[i + 1] - 32;
