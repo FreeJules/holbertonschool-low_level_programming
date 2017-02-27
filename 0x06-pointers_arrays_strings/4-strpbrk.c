a6557e2f6a574ad27442b9115c62b67978a5daad
Julija Lee <lee.julija@gmail.com>
2017-02-27 21:16:39 +0000

    changed 4 to do it by lenght because of '\0'

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to s
+ *
+ * Return: 0 on success
+ *
+ */
+int _strlen(char *s)
+{
+	int count = 0;
 
+	if (s != '\0')
+	{
+		while (*(s + count) != '\0')
+			count++;
+	}
+	return (count);
+}
 /**
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string
@@ -9,9 +26,10 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i, j, len;
 
-	for (i = 0; s[i] != '\0'; i++)
+	len = _strlen(s);
+	for (i = 0; i <= len; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
@@ -19,5 +37,5 @@ char *_strpbrk(char *s, char *accept)
 				return (s + i);
 		}
 	}
-	return (s + i + 1);
+	return (s + i);
 }
