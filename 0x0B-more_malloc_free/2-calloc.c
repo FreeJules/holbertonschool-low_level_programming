1d84a83d36ed459eb44a2b2d515f78471f8516b0
Julija Lee <lee.julija@gmail.com>
2017-03-10 04:22:56 +0000

    forgot to initialize in 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -11,12 +11,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *array;
+	int *array;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(sizeof(size) * nmemb);
 	if (array == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		array[i] = 0;
 	return (array);
 }
