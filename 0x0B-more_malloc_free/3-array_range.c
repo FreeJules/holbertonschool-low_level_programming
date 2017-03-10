b291d17f53665cf85bedc9b31148312b42e3b776
Julija Lee <lee.julija@gmail.com>
2017-03-10 04:45:18 +0000

    adding 4

---
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * array_range - creates an array of integers
+ * @min: starting value including min
+ * @max: ending value including max
+ *
+ * Return: pointer to new array
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+	int i, j;
+
+	if (min > max)
+		return (NULL);
+	array = malloc(sizeof(int) * max - min + 1);
+	if (array == NULL)
+		return (NULL);
+	for (i = min, j = 0; i <= max; i++, j++)
+		array[j] = i;
+	return (array);
+}
