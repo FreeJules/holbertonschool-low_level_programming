fcef7c00ee7d7572e3eb4b984abe74c9fcaeff7b
Julija Lee <lee.julija@gmail.com>
2017-03-14 01:33:11 +0000

    fixed freeing on 4, ugh... can't belive I forgot it

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -20,6 +20,27 @@ int _strlen(char *s)
 	return (count);
 }
 /**
+ * _strcpy - copies the string with \0 to the buffer
+ * @dest: pointer to dest
+ * @src: pointer to string
+ *
+ * Return: the pointer to dest
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+/**
  * new_dog - creates a new dog
  * @name: name
  * @age: age
@@ -28,7 +49,7 @@ int _strlen(char *s)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, len_name, len_owner;
+	int len_name, len_owner;
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
@@ -37,17 +58,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len_name = _strlen(name);
 	new_dog->name = malloc(sizeof(char) * (len_name + 1));
 	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	for (i = 0; i < len_name; i++)
-		new_dog->name[i] = name[i];
-	new_dog->name[i] = '\0';
+	}
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
 	len_owner = _strlen(owner);
 	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
-	for (i = 0; i < len_owner; i++)
-		new_dog->owner[i] = owner[i];
-	new_dog->owner[i] = '\0';
+	}
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
