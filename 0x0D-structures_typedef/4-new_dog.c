f6982f2b69016d9f94fc9bf756ea5b88454d400e
Julija Lee <lee.julija@gmail.com>
2017-03-14 01:21:19 +0000

    fixed 4 and 5, maybe

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to s
+ *
+ * Return: 0 on success
+ *
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s != '\0')
+	{
+		while (*(s + count) != '\0')
+			count++;
+	}
+	return (count);
+}
+/**
  * new_dog - creates a new dog
  * @name: name
  * @age: age
@@ -10,13 +28,26 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, len_name, len_owner;
 	struct dog *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (new_dog->name == NULL)
+		return (NULL);
+	for (i = 0; i < len_name; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
 	new_dog->age = age;
-	new_dog->owner = owner;
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (new_dog->owner == NULL)
+		return (NULL);
+	for (i = 0; i < len_owner; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 	return (new_dog);
 }
