18874d9eb72c56cdc8ab2dea3a2fde68cbbbb71d
Julija Lee <lee.julija@gmail.com>
2017-03-16 21:14:28 +0000

    adding 100

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
+/**
+ * main - check the code for Holberton School students.
+ * argc: number of args
+ * argv: pointers to string args
+ * Return: Always 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int n, i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit (1);
+	}
+	n  = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit (2);
+	}
+	for (i = 0; i < n; i++)
+		printf("%x ", ((unsigned char *)main)[i]);
+	printf("\n");
+	return (0);
+}
