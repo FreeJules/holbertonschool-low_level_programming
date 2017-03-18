5361d34736b94d649875e99f01a53d5d417b4683
Julija Lee <lee.julija@gmail.com>
2017-03-18 20:03:32 +0000

    had typos

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,9 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTIONS_H
+void _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 /**
  * struct format - list of types of arguments passed to the function
  *
- * @: The operator
+ * @c: The operator
  * @f: The function associated
  */
 typedef struct format
@@ -11,9 +16,4 @@ typedef struct format
 	char c;
 	void (*f)();
 } format_t;
-void _putchar(char);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 #endif
