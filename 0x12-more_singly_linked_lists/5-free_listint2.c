06f041db0660d294f15c27d2b980ed9401cacfc1
Julija Lee <lee.julija@gmail.com>
2017-03-28 17:22:19 +0000

    added check if tmp == NULL and while (1) loop

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -6,22 +6,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	free_listint(*head);
-	*head = NULL;
-}
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to first node
- * Return: none
- */
-void free_listint(listint_t *head)
-{
 	listint_t *tmp;
 
-	while (head)
+	while (1)
 	{
-		tmp = head;
-		head = tmp->next;
+		tmp = *head;
+		if (tmp == NULL)
+			break;
+		*head = tmp->next;
 		free(tmp);
 	}
+	*head = NULL;
 }
