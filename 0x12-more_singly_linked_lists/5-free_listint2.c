8d68b754f93dc42f8f61ffe5f6ddaccf138652ee
Julija Lee <lee.julija@gmail.com>
2017-03-28 17:16:52 +0000

    added free_listint

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,22 @@
  */
 void free_listint2(listint_t **head)
 {
+	free_listint(*head);
+	*head = NULL;
+}
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to first node
+ * Return: none
+ */
+void free_listint(listint_t *head)
+{
 	listint_t *tmp;
 
-	while (*head)
+	while (head)
 	{
-		tmp = *head;
-		*head = tmp->next;
+		tmp = head;
+		head = tmp->next;
 		free(tmp);
 	}
-	*head = NULL;
 }
