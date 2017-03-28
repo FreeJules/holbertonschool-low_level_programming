532e370158fff308b9fa1ea4edd8d5ea173e3d0a
Julija Lee <lee.julija@gmail.com>
2017-03-28 22:12:37 +0000

    removed *head=NULL because it already does it in the loop

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL || head == NULL)
-		return;
 	while (*head)
 	{
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
 	}
-	*head = NULL;
 }
