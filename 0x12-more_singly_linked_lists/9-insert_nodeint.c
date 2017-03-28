3d4da4586df159ca94eff5cd323c348a01849f08
Julija Lee <lee.julija@gmail.com>
2017-03-28 18:12:24 +0000

    changed *head to head

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,15 +16,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	tmp = *head;
-	if (*head == NULL && idx != 0)
+
+	if (head == NULL && idx != 0)
 		return (NULL);
 	if (idx == 0)
 	{
-		new->next = tmp->next;
+		new->next = *head;
 		*head = new;
 		return (&*new);
 	}
+
+	tmp = *head;
 	i = 0;
 	while (tmp)
 	{
