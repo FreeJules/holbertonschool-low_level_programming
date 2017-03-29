5b079d1f8784984927f3693b0e1319778e416817
Julija Lee <lee.julija@gmail.com>
2017-03-29 20:54:05 +0000

    removed &*

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
-		return (&*new);
+		return (new);
 	}
 
 	tmp = *head;
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = tmp->next;
 			tmp->next = new;
-			return (&*new);
+			return (new);
 		}
 		tmp = tmp->next;
 		i++;
