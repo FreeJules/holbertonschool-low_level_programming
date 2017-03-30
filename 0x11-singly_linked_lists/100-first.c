005152405d75e7e3f49e487ab7751f310ae2e8d4
Julija Lee <lee.julija@gmail.com>
2017-03-30 18:32:23 +0000

    adding 100

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_firat - two sentences to print before main
+ * Return: none
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
