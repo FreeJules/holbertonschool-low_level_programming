6c7686c2c840c883eefe444c89d6b47198af2cb7
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:03:09 +0000

    emoved check from if text_c ++ NULL

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -40,8 +40,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		if (close(fd) == -1)
-			return (-1);
+		close(fd);
 		return (1);
 	}
 	len = _strlen(text_content);
