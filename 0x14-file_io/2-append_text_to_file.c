7905bc4b9990cdedcda3172d525926a525069679
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:10:53 +0000

    removed permissions

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -31,13 +31,12 @@ int _strlen(char *s)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_written;
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content ==  NULL)
 		return (1);
-	fd = open(filename, O_WRONLY | O_APPEND, mode);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	len = _strlen(text_content);
