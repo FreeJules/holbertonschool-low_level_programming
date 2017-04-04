b0909deb478c13e09bbb9ff842cf54f4d6b94394
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:15:34 +0000

    moved close(fd) outside of if statement

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -41,7 +41,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	len = _strlen(text_content);
 	bytes_written = write(fd, text_content, len);
-	if (close(fd) == -1 || bytes_written == -1)
+	close(fd);
+	if (bytes_written == -1)
 		return (-1);
 	return (1);
 }
