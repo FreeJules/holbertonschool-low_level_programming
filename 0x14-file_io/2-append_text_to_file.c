3fad4dd5f44a4450be9a2719e7e289173b65e48b
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:09:23 +0000

    switched RDWR to O_WRONLY

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -37,7 +37,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content ==  NULL)
 		return (1);
-	fd = open(filename, O_RDWR | O_APPEND, mode);
+	fd = open(filename, O_WRONLY | O_APPEND, mode);
 	if (fd == -1)
 		return (-1);
 	len = _strlen(text_content);
