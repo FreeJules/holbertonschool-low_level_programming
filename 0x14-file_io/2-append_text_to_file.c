a0de78cb34b7ae853addb3342cb0e76ff269ef1b
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:23:24 +0000

    increased lenght by 1

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -43,7 +43,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 	len = _strlen(text_content);
-	bytes_written = write(fd, text_content, len);
+	bytes_written = write(fd, text_content, len + 1);
 	close(fd);
 	if (bytes_written == -1)
 		return (-1);
