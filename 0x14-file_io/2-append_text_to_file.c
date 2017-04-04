aada4aaf6410d523809f3dfd52b2c153c7160f2b
Julija Lee <lee.julija@gmail.com>
2017-04-04 21:18:28 +0000

    moved stuff around

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -30,15 +30,18 @@ int _strlen(char *s)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, bytes_written;
+	ssize_t fd, len, bytes_written;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content ==  NULL)
-		return (1);
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content ==  NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	len = _strlen(text_content);
 	bytes_written = write(fd, text_content, len);
 	close(fd);
