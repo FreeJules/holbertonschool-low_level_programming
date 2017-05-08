54691d4a847d539595725c19e1ff14471179a455
Julija Lee <lee.julija@gmail.com>
2017-05-08 15:29:27 +0000

    added NULL check for head

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,6 +12,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 
 	i = 0;
+	if (head == NULL || *head == NULL)
+		return (-1);
 	tmp = *head;
 	while (tmp)
 	{
