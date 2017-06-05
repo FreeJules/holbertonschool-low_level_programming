b13d532ca6281810fc74ef00cda742f3fa487e9c
Julija Lee <lee.julija@gmail.com>
2017-06-05 22:59:40 +0000

    fixed empty hash

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -8,12 +8,17 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp;
+	int flag = 1;
 
 	if (!ht)
 		return;
-	putchar('{');
 	for (i = 0; i < ht->size; i++)
 	{
+		if (ht->array[i] && flag)
+		{
+			putchar('{');
+			flag = 0;
+		}
 		tmp = ht->array[i];
 		while (tmp)
 		{
@@ -25,6 +30,7 @@ void hash_table_print(const hash_table_t *ht)
 		if (ht->array[i + 1] && i != ht->size - 1)
 			printf(", ");
 	}
-	putchar('}');
+	if (!flag)
+		putchar('}');
 	putchar('\n');
 }
