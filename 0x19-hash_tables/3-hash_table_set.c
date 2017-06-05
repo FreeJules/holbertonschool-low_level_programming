c62dacd0c9453cf7ca852da25a22ebc3bf332656
Julija Lee <lee.julija@gmail.com>
2017-06-05 21:07:33 +0000

    was missing check for empty key

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *tmp;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
