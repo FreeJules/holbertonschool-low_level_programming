bbf484927192ce00b50e30f94e19458ae7c6ceaf
Julija Lee <lee.julija@gmail.com>
2017-06-05 21:57:04 +0000

    switched order in strcmp

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	tmp = ht->array[index];
 	while (tmp)
 	{
-		if (strcmp(tmp->key, key) == 0)
+		if (strcmp(key, tmp->key) == 0)
 		{
 			free(tmp->value);
 			tmp->value = strdup(value);
