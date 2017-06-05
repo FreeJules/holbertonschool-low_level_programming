96322f186bba4a8e283623d2ba6a67b897c731dc
Julija Lee <lee.julija@gmail.com>
2017-06-05 22:44:35 +0000

    adding 5

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ * Return: Always EXIT_SUCCESS.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			printf("\'%s\': \'%s\'", tmp->key, tmp->value);
+			if (tmp->next)
+				printf(", ");
+			tmp = tmp->next;
+		}
+		if (ht->array[i + 1] && i != ht->size - 1)
+			printf(", ");
+	}
+	putchar('}');
+	putchar('\n');
+}
