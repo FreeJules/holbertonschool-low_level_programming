a9684494f15f6912626fd9cfea75f36922dea672
Julija Lee <lee.julija@gmail.com>
2017-06-21 21:19:45 +0000

    fixed betty style

--- 0x1A-sorting_algorithms/100-shell_sort.c
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -15,26 +15,26 @@ void shell_sort(int *array, size_t size)
 
 	int nElems = size;
 
-	while(h <= nElems/3)
+	while (h <= nElems / 3)
 	{
-		h = h*3 + 1;
+		h = h * 3 + 1;
 	}
-	while(h>0)
+	while (h > 0)
 	{
 		if (h == 1)
 			print_array(array, size);
-		for(i=h; i<nElems; i++)
+		for (i = h; i < nElems; i++)
 		{
 			temp = array[i];
-		       	j = i;
-	       		while(j > h-1 && array[j-h] >= temp)
-	       		{
-	       			array[j] = array[j-h];
-		       		j -= h;
-		       	}
+			j = i;
+			while (j > h - 1 && array[j - h] >= temp)
+			{
+				array[j] = array[j - h];
+				j -= h;
+			}
 			array[j] = temp;
-	       	}
-		h = (h-1) / 3;
+		}
+		h = (h - 1) / 3;
 	}
 	print_array(array, size);
 }
